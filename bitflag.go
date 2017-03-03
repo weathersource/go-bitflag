c69724e9c6268031cb0687546adee892ddf060de
Albulescu Cosmin <albulescu.cosmin@gmail.com>
2017-03-03 22:04:00 +0200

    Update bitflag.go

--- bitflag.go
+++ bitflag.go
@@ -1,3 +1,7 @@
+// Copyright 2017 Cosmin Albulescu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package bitflag
 
 // Set flag : Set(&flag, FLAG_B, FLAG_C)
