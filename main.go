a6589e5a8cef228013dd411d1e6a0a82eb1b4da4
Cosmin Albulescu <cosmin@albulescu.ro>
2017-03-03 21:21:50 +0200

    Add bitflag source

---
+++ main.go
@@ -0,0 +1,39 @@
+package flag
+
+// Set flag : Set(&flag, FLAG_B, FLAG_C)
+func Set(f *uint, opts ...uint) {
+	for _, o := range opts {
+		*f |= (1 << o)
+	}
+}
+
+// Unset flag : Unset(&flag, FLAG_B, FLAG_C)
+func Unset(f *uint, opts ...uint) {
+	for _, o := range opts {
+		*f = ^(1 << o)
+	}
+}
+
+// Flag is setted : Isset(&flag, FLAG_A), Isset(&flag, FLAG_A, FLAG_B), Isset(&flag, FLAG_A | FLAG_B)
+func Isset(f uint, opts ...uint) (isset bool) {
+
+	for _, o := range opts {
+		if (f & (1 << o)) == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// One of opts is setted in flag : OneOf(flag, FLAG_A, FLAG_B)
+func OneOf(f uint, opts ...uint) (isset bool) {
+
+	for _, o := range opts {
+		if (f & (1 << o)) > 0 {
+			return true
+		}
+	}
+
+	return
+}
